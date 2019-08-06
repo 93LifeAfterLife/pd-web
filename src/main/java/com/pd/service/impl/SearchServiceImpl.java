e5798dbf24ae7bf95264987ca1ef33c83d47d86b
93LifeAfterLife <tangdingjnust@163.com>
2019-08-06 19:45:36 +0800

    pd-web project

---
+++ src/main/java/com/pd/service/impl/SearchServiceImpl.java
@@ -0,0 +1,40 @@
+package com.pd.service.impl;
+
+import java.util.List;
+
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.SolrQuery;
+import org.apache.solr.client.solrj.response.QueryResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.pd.pojo.Item;
+import com.pd.service.SearchService;
+
+@Service
+public class SearchServiceImpl implements SearchService {
+	/**
+	 * SolrClient实例是在  SolrAutoConfiguration 类中创建的
+	 * 
+	 * SolrAutoConfiguration 添加了 @Configuration 注解
+	 * 	是spring boot 自动配置类, 其中的 solrClient() 方法中创建了SolrClient 实例 
+	 */
+	@Autowired
+	private SolrClient solrClient;
+	
+	@Override
+	public List<Item> findItemByKey(String key) throws Exception {
+		//封装查询的关键词
+		//也可以封装其他的查询参数, 比如指定字段, facet设置等
+		SolrQuery query = new SolrQuery(key);
+		//查询前多少条数据
+		query.setStart(0);
+		query.setRows(20);
+		
+		//执行查询并得到查询结果
+		QueryResponse qr = solrClient.query(query);
+		//把查询结果转成一组商品实例
+		List<Item> beans = qr.getBeans(Item.class);
+		return beans;
+	}
+}
